dc727abbd42b453de1d1890d9dee0f0451425c46
Taeyoon Kim <tykimseoul@gmail.com>
2019-09-05 14:24:35 +0900

    add: basic ui elements

--- app/src/main/java/com/example/pc/dijkstraatkaist/AdminActivity.kt
+++ app/src/main/java/com/example/pc/dijkstraatkaist/AdminActivity.kt
@@ -1,12 +1,57 @@
 package com.example.pc.dijkstraatkaist
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import kotlinx.android.synthetic.main.activity_admin.*
 
-class AdminActivity : AppCompatActivity() {
+class AdminActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: NaverMap) {
+        Toast.makeText(this, "map ready", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
